6e2d350ebceace131ec1cdbf5f954606c4e92c25
MathGD <m.vancappellen@rubika-edu.com>
2022-03-03 14:50:57 +0100

    Modification CursorMode

---
+++ NeedForTuning/Assets/Scripts/CursorController.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CursorController : MonoBehaviour
+{
+    public Texture2D cursor;
+    public Texture2D cursorClicked;
+
+    private void Awake()
+    {
+        ChangeCursor(cursor);
+        Cursor.lockState = CursorLockMode.Confined;
+    }
+
+    private void ChangeCursor(Texture2D cursorType)
+    {
+        //Vector2 hotspot = new Vector2(cursorType.width / 2, cursorType.height / 2);
+        Cursor.SetCursor(cursorType, Vector2.zero, CursorMode.Auto);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
