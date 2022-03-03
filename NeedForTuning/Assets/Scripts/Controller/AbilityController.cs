1073c983becaac73535e28c8ae7fdc60da72e1f2
Antoine <a.leroux@rubika-edu.com>
2022-03-03 15:28:57 +0100

    Added ability Controller

--- NeedForTuning/Assets/Scripts/Controller/AbilityController.cs
+++ NeedForTuning/Assets/Scripts/Controller/AbilityController.cs
@@ -4,15 +4,110 @@ using UnityEngine;
 
 public class AbilityController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [HideInInspector] public CarPiece abilityEngine;
+    [HideInInspector] public CarPiece abilityTire;
+    [HideInInspector] public CarPiece abilityChassis;
+
+    private void TriggerAbility(PieceAbility ability)
     {
-        
+        switch (abilityEngine.ability.name)
+        {
+            case "Spring":
+                Spring(ability);
+                break;
+            case "AutoGearbox":
+                AutoGearbox(ability);
+                break;
+            case "Bumper":
+                Bumper(ability);
+                break;
+            case "Dolorean":
+                Dolorean(ability);
+                break;
+            case "Grip":
+                Grip(ability);
+                break;
+            case "Nail":
+                Nail(ability);
+                break;
+            case "Suspension":
+                Suspension(ability);
+                break;
+            case "Swim":
+                Swim(ability);
+                break;
+            case "Turbo":
+                Turbo(ability);
+                break;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    #region Trigger Ability
+    private void ClickEngine()
     {
-        
+        TriggerAbility(abilityEngine.ability);
     }
+
+    private void ClickTire()
+    {
+        TriggerAbility(abilityTire.ability);
+    }
+
+
+    private void ClickChassis()
+    {
+        TriggerAbility(abilityChassis.ability);
+    }
+    #endregion
+
+    #region Engine Ability
+    private void Grip(PieceAbility ability)
+    {
+
+    }
+
+    private void AutoGearbox(PieceAbility ability)
+    {
+
+    }
+
+    private void Turbo(PieceAbility ability)
+    {
+
+    }
+    #endregion
+
+    #region Tire Ability
+    private void Spring(PieceAbility ability)
+    {
+
+    }
+
+    private void Dolorean(PieceAbility ability)
+    {
+
+    }
+
+    private void Nail(PieceAbility ability)
+    {
+
+    }
+    #endregion
+
+    #region Chassis Ability
+    private void Bumper(PieceAbility ability)
+    {
+
+    }
+
+    private void Swim(PieceAbility ability)
+    {
+
+    }
+
+    private void Suspension(PieceAbility ability)
+    {
+
+    }
+    #endregion
 }
