1f611230bd9c008369a6d71673676c0938423707
Lucas D <l.deutschmann@rubika-edu.com>
2022-03-03 16:38:07 +0100

    AddedTurbo

--- NeedForTuning/Assets/Scripts/Controller/AbilityController.cs
+++ NeedForTuning/Assets/Scripts/Controller/AbilityController.cs
@@ -2,46 +2,92 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+enum Abilities {Spring,AutoGearbox,Dolorean,Grip,Nail,Swim,Suspension,Turbo,Bumper}
 public class AbilityController : MonoBehaviour
 {
+    private CarController car;
     [HideInInspector] public CarPiece abilityEngine;
     [HideInInspector] public CarPiece abilityTire;
     [HideInInspector] public CarPiece abilityChassis;
+    private Abilities currentAbility;
 
+    [Header ("Abilities")]
+    public float turboMultiplier;
+    private void Start()
+    {
+        car = GetComponent<CarController>();
+    }
+    
     private void TriggerAbility(PieceAbility ability)
     {
         switch (abilityEngine.ability.name)
         {
             case "Spring":
                 Spring(ability);
+                currentAbility = Abilities.Spring;
                 break;
             case "AutoGearbox":
                 AutoGearbox(ability);
+                currentAbility = Abilities.AutoGearbox;
                 break;
             case "Bumper":
                 Bumper(ability);
+                currentAbility = Abilities.Bumper;
                 break;
             case "Dolorean":
                 Dolorean(ability);
+                currentAbility = Abilities.Dolorean;
                 break;
             case "Grip":
                 Grip(ability);
+                currentAbility = Abilities.Grip;
                 break;
             case "Nail":
                 Nail(ability);
+                currentAbility = Abilities.Nail;
                 break;
             case "Suspension":
                 Suspension(ability);
+                currentAbility = Abilities.Suspension;
                 break;
             case "Swim":
                 Swim(ability);
+                currentAbility = Abilities.Swim;
                 break;
             case "Turbo":
-                Turbo(ability);
+                Turbo(ability,true);
+                currentAbility = Abilities.Turbo;
                 break;
         }
     }
+    
+    public void StopAbility()
+    {
+        switch (currentAbility)
+        {
+            case Abilities.Spring:
+            break;
+            case Abilities.AutoGearbox:
+            break;
+            case Abilities.Bumper:
+            break;
+            case Abilities.Dolorean:
+            break;
+            case Abilities.Grip:
+            break;
+            case Abilities.Nail:
+            break;
+            case Abilities.Suspension:
+            break;
+            case Abilities.Swim:
+            break;
+            case Abilities.Turbo:
+            Turbo(null,false);
+            break;
 
+            
+        }
+    }
     #region Trigger Ability
     private void ClickEngine()
     {
@@ -71,9 +117,19 @@ public class AbilityController : MonoBehaviour
 
     }
 
-    private void Turbo(PieceAbility ability)
+    private void Turbo(PieceAbility ability,bool enabled)
     {
-
+        if (enabled)
+        {
+            ChunkManager.Instance.modulesToCross = ChunkManager.Instance.totalNbOfLineActu + ability.moduleDistance;
+            
+            ChunkManager.Instance.speedActu = car.engineMaxSpeed * turboMultiplier;
+        }
+        else
+        {
+            ChunkManager.Instance.speedActu = car.engineMaxSpeed;
+        }
+        
     }
     #endregion
 
