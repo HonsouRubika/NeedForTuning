767cd492d5c1ce6c1477360a085b3c3edb96f02c
Lucas D <l.deutschmann@rubika-edu.com>
2022-03-03 11:47:16 +0100

    Car Pieces Done

--- NeedForTuning/Assets/Scripts/ScriptablesObjects/CarPiece.cs
+++ NeedForTuning/Assets/Scripts/ScriptablesObjects/CarPiece.cs
@@ -8,8 +8,40 @@ public class CarPiece : ScriptableObject
     public new string name;
     public Sprite image;
 
-    public int maxSpeed;
-    public int acceleration;
-    public int adhesion;
+    [Header("MaxSpeed")]
+    [Range(0,5)]
+    public int maxSpeedConcrete;
+    [Range(0, 5)]
+    public int maxSpeedSand;
+    [Range(0, 5)]
+    public int maxSpeedIce;
+    [Range(0, 5)]
+    public int maxSpeedBump;
+
+    [Header("Acceleration")]
+    [Range(0, 5)]
+    public int accelerationConcrete;
+    [Range(0, 5)]
+    public int accelerationSand;
+    [Range(0, 5)]
+    public int accelerationIce;
+    [Range(0, 5)]
+    public int accelerationBump;
+
+    [Header("Adhesion")]
+    [Range(0, 5)]
+    public int adhesionConcrete;
+    [Range(0, 5)]
+    public int adhesionSand;
+    [Range(0, 5)]
+    public int adhesionIce;
+    [Range(0, 5)]
+    public int adhesionBump;
+
+    [Header("Resistance")]
+    [Range(0, 5)]
     public int resistance;
+    
+
+    
 }
