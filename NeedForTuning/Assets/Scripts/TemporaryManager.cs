c17ab6ddcc76f45e0e48b944a4e88108c775c1d3
MathGD <m.vancappellen@rubika-edu.com>
2022-03-04 15:41:20 +0100

    Black Screen Animation Finish

--- NeedForTuning/Assets/Scripts/TemporaryManager.cs
+++ NeedForTuning/Assets/Scripts/TemporaryManager.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class TemporaryManager : MonoBehaviour
 {
     public GameObject clickableStartText;
+    public GameObject blackScreen;
 
     // Start is called before the first frame update
     void Start()
@@ -16,7 +17,16 @@ public class TemporaryManager : MonoBehaviour
     {
         Animator anim = clickableStartText.GetComponent<Animator>();
         anim.SetBool("clickToStart", true);
-        Debug.Log("Starting Game");
+        StartCoroutine(TransitionBlackScreen());
+    }
+
+    IEnumerator TransitionBlackScreen()
+    {
+        yield return new WaitForSeconds(1f);
+        Animator anim = blackScreen.GetComponent<Animator>();
+        anim.SetBool("fadeInBlackScreen", true);
+        yield return new WaitForSeconds(2f);
+        anim.SetBool("fadeInBlackScreen", false);
     }
 
     // Update is called once per frame
